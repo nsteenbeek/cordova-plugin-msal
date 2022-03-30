e70b92b937e110dcae1b28d1952caa2b83d5d806
Niels Steenbeek <nsteenbeek@hso.com>
2022-03-30 13:37:38 +0200

    Cordova-windows support

--- src/scripts/after_prepare.js
+++ src/scripts/after_prepare.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 
-const config = JSON.parse(fs.readFileSync('platforms/browser/browser.json').toString());
-const CLIENT_ID = `'${config.installed_plugins["cordova-plugin-msal"].CLIENT_ID}'`;
-const TENANT_ID = `'${config.installed_plugins["cordova-plugin-msal"].TENANT_ID}'`;
-
 // If our application has the browser platform installed, this is ideally where we want to configure it.
 if (fs.existsSync('platforms/browser/browser.json')) {
+    const config = JSON.parse(fs.readFileSync('platforms/browser/browser.json').toString());
+    const CLIENT_ID = `'${config.installed_plugins["cordova-plugin-msal"].CLIENT_ID}'`;
+    const TENANT_ID = `'${config.installed_plugins["cordova-plugin-msal"].TENANT_ID}'`;
     const proxyFile = 'platforms/browser/www/plugins/cordova-plugin-msal/src/browser/MsalProxy.js';
     const contents = fs.readFileSync(proxyFile).toString();
     fs.writeFileSync(proxyFile, contents.replace(/CLIENT_ID/g, CLIENT_ID).replace(/TENANT_ID/g, TENANT_ID));
@@ -13,6 +12,9 @@ if (fs.existsSync('platforms/browser/browser.json')) {
 
 // If our application has the windows platform installed, this is ideally where we want to configure it.
 if (fs.existsSync('platforms/windows/windows.json')) {
+    const config = JSON.parse(fs.readFileSync('platforms/windows/windows.json').toString());
+    const CLIENT_ID = `'${config.installed_plugins["cordova-plugin-msal"].CLIENT_ID}'`;
+    const TENANT_ID = `'${config.installed_plugins["cordova-plugin-msal"].TENANT_ID}'`;
     const proxyFile = 'platforms/windows/www/plugins/cordova-plugin-msal/src/windows/MsalProxy.js';
     const contents = fs.readFileSync(proxyFile).toString();
     fs.writeFileSync(proxyFile, contents.replace(/CLIENT_ID/g, CLIENT_ID).replace(/TENANT_ID/g, TENANT_ID));
